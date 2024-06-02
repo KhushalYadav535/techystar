3f207d3823f5996b926e12af34865b139e28a0f1
KhushalYadav535 <khushalyadav535@gmail.com>
2024-06-02 13:52:12 +0530

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>TechyStar</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+          Welcome to our website! We are a team of dedicated developers specializing in web and software development, with expertise in HTML, CSS, JavaScript, ReactJS, NodeJS, MongoDB, and C++. Our diverse skill set allows us to create dynamic, robust, and scalable applications tailored to meet your unique needs. Whether you require a sleek and responsive website, a powerful backend system, or a complex software solution, we have the knowledge and experience to deliver exceptional results. Explore our services and see how we can help bring your vision to life.
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
